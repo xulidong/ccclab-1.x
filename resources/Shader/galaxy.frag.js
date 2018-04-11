8f2496c1065853cc47c69af77d136a1057152b78
xulidong <xu.lidong@qq.com>
2018-04-11 22:23:11 +0800

    流光

--- resources/Shader/galaxy.frag.js
+++ resources/Shader/galaxy.frag.js
@@ -9,7 +9,7 @@ void main()
 {
     vec4 src_color = texture2D(CC_Texture0, v_texCoord).rgba;
     float width = 0.2; // 流光的宽度范围(调整该值改变流光的宽度)
-    float start = sys_time; // 流光的起始x坐标
+    float start = sys_time * 1.2; // 流光的起始x坐标
     float strength = 0.02; // 流光增亮强度(调整该值改变流光的增亮强度)
     float offset = 0.2; // 偏移值(调整该值改变流光的倾斜程度)
     
